d2593716fad2aebb19e23bd9b18f3f06ee544640
pauldambra <paul.dambra@gmail.com>
2018-03-04 22:09:42 +0000

    really remove all of the code expect for the walking skeleton

--- destinations/NoNameProvided.js
+++
@@ -1,6 +0,0 @@
-module.exports = class NoNameProvided extends Error {
-  constructor (...args) {
-    super(...args)
-    Error.captureStackTrace(this, NoNameProvided)
-  }
-}
--- proposeDestination.js
+++ proposeDestination.js
@@ -1,24 +1,8 @@
 
-const map = require('./destinations/map')
-const commandHandler = require('./destinations/commandHandler')
-const guid = require('./GUID')
-
-let streamRepo
-const dynamoDbClient = require('./destinations/dynamoDbClient')
-const makeStreamRepository = require('./destinations/make-stream-repository')
-
 exports.handler = (event, context, callback) => {
-  const proposeDestination = map.fromAPI(event, 'proposeDestination')
-
-  streamRepo = streamRepo || makeStreamRepository.for(dynamoDbClient.connect(), guid)
-
-  commandHandler
-    .apply({
-      command: proposeDestination,
-      type: 'destinationProposed',
-      streamRepository: streamRepo,
-      guidGenerator: guid // because command handler knows about correlation id o_O
-    })
-    .then(() => map.toSuccessResponse(proposeDestination, callback))
-    .catch((err) => map.toResponseForInvalidRequest(err, proposeDestination, callback))
+  console.log(`received event: ${JSON.stringify(event)}`)
+  callback(null, {
+    statusCode: 200,
+    body: 'OK'
+  })
 }
