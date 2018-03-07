7ea4982c160f5b9f80a715d2e7be8ff676d8d8e4
pauldambra <paul.dambra@gmail.com>
2018-03-07 20:29:50 +0000

    update dynamodb table creation script

--- createDynamoTable.sh
+++ createDynamoTable.sh
@@ -5,7 +5,7 @@ aws dynamodb create-table \
   --table-name visitplannr-events \
   --attribute-definitions \
         AttributeName=EventId,AttributeType=S AttributeName=StreamName,AttributeType=S \
-  --key-schema AttributeName=EventId,KeyType=HASH AttributeName=StreamName,KeyType=RANGE \
+  --key-schema AttributeName=StreamName,KeyType=HASH AttributeName=EventId,KeyType=RANGE \
   --provisioned-throughput \
   ReadCapacityUnits=5,WriteCapacityUnits=5 \
   --endpoint-url http://0.0.0.0:8000
