463a46c31f2f9d00ee6d28183fe2642cb7615ffe
pauldambra <paul.dambra@gmail.com>
2018-03-07 23:33:06 +0000

    improve stream repo tests

--- test/persistence.spec.js
+++ test/persistence.spec.js
@@ -2,31 +2,63 @@ const streamRepoFactory = require('../destinations/make-stream-repository')
 const expect = require('chai').expect
 
 describe('the stream repository', function () {
-  it('decorates the event when writing to the stream', function (done) {
-    const fakeGuidGenerator = {
-      generate: () => 'a-generated-guid'
-    }
+  const fakeGuidGenerator = {
+    generate: () => 'a-generated-guid'
+  }
 
-    const fakeDynamoDbClient = {
-      put: (params, callback) => { callback() }
-    }
+  const fakeDynamoDbClient = {
+    put: (params, callback) => { callback() }
+  }
 
-    const streamRepo = streamRepoFactory.for(fakeDynamoDbClient, fakeGuidGenerator)
+  const streamRepo = streamRepoFactory.for(fakeDynamoDbClient, fakeGuidGenerator)
 
-    streamRepo.writeToStream(
+  let writeToTheStream
+
+  beforeEach(function () {
+    writeToTheStream = streamRepo.writeToStream(
       {
         streamName: 'arbitrary-string',
-        event: {test: 'poo'}
+        event: {winnie: 'pooh'}
       }
-    ).then(writtenItem => {
-      expect(writtenItem.TableName).to.equal('visitplannr-events')
-      expect(writtenItem.Item.StreamName).to.equal('arbitrary-string-a-generated-guid')
-      expect(writtenItem.Item.EventId).to.equal('a-generated-guid')
-      expect(writtenItem.Item.event).to.deep.equal({
-        test: 'poo',
-        correlationId: 'a-generated-guid'
+    )
+  })
+
+  it('decorates the event with a correlation id', function (done) {
+    writeToTheStream
+      .then(writtenItem => {
+        expect(writtenItem.Item.event).to.deep.equal({
+          winnie: 'pooh',
+          correlationId: 'a-generated-guid'
+        })
+        done()
       })
-      done()
-    })
+      .catch(done)
+  })
+
+  it('writes to the expected table', function (done) {
+    writeToTheStream
+      .then(writtenItem => {
+        expect(writtenItem.TableName).to.equal('visitplannr-events')
+        done()
+      })
+      .catch(done)
+  })
+
+  it('adds the correlation id to the stream name', function (done) {
+    writeToTheStream
+      .then(writtenItem => {
+        expect(writtenItem.Item.StreamName).to.equal('arbitrary-string-a-generated-guid')
+        done()
+      })
+      .catch(done)
+  })
+
+  it('writes an event id guid', function (done) {
+    writeToTheStream
+      .then(writtenItem => {
+        expect(writtenItem.Item.EventId).to.equal('a-generated-guid')
+        done()
+      })
+      .catch(done)
   })
 })
