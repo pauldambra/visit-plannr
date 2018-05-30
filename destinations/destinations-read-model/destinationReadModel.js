7af83a942954d3213f3c94ef0b726783416215c2
pauldambra <paul.dambra@gmail.com>
2018-05-30 22:33:48 +0100

    hydrate the destination read model using a switch statement

--- destinations/destinations-read-model/destinationReadModel.js
+++ destinations/destinations-read-model/destinationReadModel.js
@@ -1,26 +1,19 @@
 
-const applyDestinationProposed = (model, event) => {
-  if (event.type === 'destinationProposed') {
-    model.name = event.name
-    model.geolocation = event.geolocation
-  }
-  return model
-}
-
-const applyGeolocationValidationResult = (model, event) => {
-  if (event.type === 'geolocationValidationSucceeded') {
-    model.status = 'valid'
-  }
-  if (event.type === 'geolocationValidationFailed') {
-    model.status = 'failed'
-  }
-  return model
-}
-
 module.exports = {
   apply: events => events.reduce((model, event) => {
-    model = applyDestinationProposed(model, event)
-    model = applyGeolocationValidationResult(model, event)
+    switch (event.type) {
+      case 'destinationProposed':
+        model.name = event.name
+        model.geolocation = event.geolocation
+        break
+      case 'geolocationValidationSucceeded':
+        model.status = 'valid'
+        break
+      case 'geolocationValidationFailed':
+        model.status = 'failed'
+        break
+    }
+
     return model
   }, {status: 'pending'})
 }
