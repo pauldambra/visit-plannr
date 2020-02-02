6d122e89d0f09cb597ca62038d20fd14e41e5233
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2020-02-02 19:14:06 +0000

    install aws cli

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,17 @@ rm -rf node_modules/
 
 npm ci --only=production
 
-pip install aws-sam-cli
+
+# install aws sam cli
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+brew tap aws/tap
+brew install aws-sam-cli
+
+# install the aws cli
 curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
