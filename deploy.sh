aa22dd7163c6d878aae490f034628ba1fabe8c61
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2020-02-02 19:04:55 +0000

    install aws cli

--- deploy.sh
+++ deploy.sh
@@ -2,37 +2,9 @@
 
 set -eux
 
-function installAWScli() {
-  curl "https://d1vvhvl2y92vvt.cloudfront.net/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  ./aws/install
-}
-
-function installAwsSam(){
-	echo "Install aws-sam-cli ${INPUT_SAM_VERSION}"
-	if [ "${INPUT_SAM_VERSION}" == "latest" ]; then
-		pip install aws-sam-cli >/dev/null 2>&1
-		if [ "${?}" -ne 0 ]; then
-			echo "Failed to install aws-sam-cli ${INPUT_SAM_VERSION}"
-		else
-			echo "Successful install aws-sam-cli ${INPUT_SAM_VERSION}"
-		fi
-	else
-		pip install aws-sam-cli==${INPUT_SAM_VERSION} >/dev/null 2>&1
-		if [ "${?}" -ne 0 ]; then
-			echo "Failed to install aws-sam-cli ${INPUT_SAM_VERSION}"
-		else
-			echo "Successful install aws-sam-cli ${INPUT_SAM_VERSION}"
-		fi
-	fi
-}
-
 rm -rf node_modules/
 
 npm ci --only=production
-
-installAwsSam()
-installAWScli()
 
 if aws s3 ls "s3://visitplannr" 2>&1 | grep -q 'NoSuchBucket'
 then
