e6f424a08f076906126e5565973f757b9ef3b1ce
Paul D'Ambra <paul.dambra@coopdigital.co.uk>
2020-02-02 18:53:39 +0000

    install aws sam

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,31 @@
 
 set -eux
 
+function installAwsSam(){
+	echo "Install aws-sam-cli ${INPUT_SAM_VERSION}"
+	if [ "${INPUT_SAM_VERSION}" == "latest" ]; then
+		pip install aws-sam-cli >/dev/null 2>&1
+		if [ "${?}" -ne 0 ]; then
+			echo "Failed to install aws-sam-cli ${INPUT_SAM_VERSION}"
+		else
+			echo "Successful install aws-sam-cli ${INPUT_SAM_VERSION}"
+		fi
+	else
+		pip install aws-sam-cli==${INPUT_SAM_VERSION} >/dev/null 2>&1
+		if [ "${?}" -ne 0 ]; then
+			echo "Failed to install aws-sam-cli ${INPUT_SAM_VERSION}"
+		else
+			echo "Successful install aws-sam-cli ${INPUT_SAM_VERSION}"
+		fi
+	fi
+}
+
 rm -rf node_modules/
 
 npm ci --only=production
+
+
+installAwsSam()
 
 if aws s3 ls "s3://visitplannr" 2>&1 | grep -q 'NoSuchBucket'
 then
